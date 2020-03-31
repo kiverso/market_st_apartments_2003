d5a9ce30d907ad9c7b5918c5b68f77dac99254af
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-31 09:34:15 -0600

    Add apartment class and tests

---
+++ lib/apartment.rb
@@ -0,0 +1,18 @@
+class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+  def initialize(apartment)
+    @number = apartment[:number]
+    @monthly_rent = apartment[:monthly_rent]
+    @bathrooms = apartment[:bathrooms]
+    @bedrooms = apartment[:bedrooms]
+    @renter = apartment[:renter]
+  end
+
+  def add_renter(renter)
+    @renter = renter
+  end
+end
