debc114237271471c9596670337a105e78274d9e
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-31 09:11:35 -0600

    Add renter class and tests

---
+++ test/renter_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require './lib/renter'
+
+class RenterTest < Minitest::Test
+  def test_it_exists
+    renter1 = Renter.new("Jessie")
+    assert_instance_of Renter, renter1
+  end
+
+  def test_it_has_readable_attributes
+    renter1 = Renter.new("Jessie")
+    assert_equal "Jessie", renter1.name
+  end
+end
